3d182d469d1d389c755a77dd4aca39a65c249345
thevi007 <211459862+brunoalexsandro@users.noreply.github.com>
2025-10-21 21:29:28 -0300

    resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.Security.Cryptography.X509Certificates;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone celular1 = new Iphone("1122203i03", "Iphone 11", "1112010003A", 128);
+Smartphone celular2 = new Nokia("8192931003", "Nokia Lumia", "131231392495E", 32);
+
+celular1.Ligar();
+celular2.ReceberLigacao();
+
+celular2.Ligar();
+celular1.ReceberLigacao();
+
+celular1.InstalarAplicativo("Pou 2");
+celular2.InstalarAplicativo("Free Fire");
